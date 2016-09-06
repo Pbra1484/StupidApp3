4af4367ae6e82b119d03defd792bd5610212f916
Brashear <pbra1484@740s-pl-108.local>
2016-09-06 13:53:01 -0600

    Runs less stupid

--- src/stupid/controller/StupidRunner.java
+++ src/stupid/controller/StupidRunner.java
@@ -1,5 +1,11 @@
 package stupid.controller;
 
-public class StupidRunner {
-
+public class StupidRunner 
+{
+	public static void main (String [ ] args)
+	{
+		StupidController stupidAppController = new StupidController();
+		stupidAppController.start();
+	}
+	
 }
